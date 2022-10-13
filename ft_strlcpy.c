c8112bca4d2ce5172e7dbadb52e9202f399c8f7f
Patryk Skrucha <pskrucha@f1r4s10.codam.nl>
2022-10-13 13:13:14 +0200

    nor fix

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:43:59 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/07 15:23:50 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/13 13:12:59 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,3 +33,10 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	return (x);
 }
 // done
+
+// int main()
+// {
+// 	char a[] = "Hello";
+// 	ft_strlcpy(a, a+2, 2);
+// 	puts(a);
+// }
