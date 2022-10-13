6bd6df2c21ff8921d2e315c9ba8a2bb3772acfb1
Patryk Skrucha <pskrucha@f1r4s10.codam.nl>
2022-10-13 12:22:08 +0200

    fixed

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 14:14:35 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/11 14:46:35 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/13 12:21:40 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	size_t		i;
-	const char	*s1;
-	const char	*s2;
+	size_t				i;
+	unsigned const char	*s1;
+	unsigned const char	*s2;
 
 	i = 0;
-	s1 = (const char *)str1;
-	s2 = (const char *)str2;
+	s1 = (unsigned const char *)str1;
+	s2 = (unsigned const char *)str2;
 	if (n == 0)
 	{
 		return (0);
 	}
-	while (s1[i] == s2[i] && (s1[i] != '\0' && s2[i] != '\0') && i < n - 1)
+	while (s1[i] == s2[i] && i < n - 1)
 	{
 			i++;
 	}
@@ -35,40 +35,36 @@ int	ft_memcmp(const void *str1, const void *str2, size_t n)
 // int main () {
 //    char str2[15];
 //    char str1[15];
-   // int ret;
+//    int ret;
 
-   // char str12[15];
-   // char str11[15];
-   // int ret1;
-   // memcpy(str1, "123456", 6);
-   // memcpy(str2, "ABCDEF", 6);
+//    char str12[15];
+//    char str11[15];
+//    int ret1;
+//    memcpy(str1, "123456", 6);
+//    memcpy(str2, "ABCDEF", 6);
 
-   // ret = memcmp(str1, str2, 5);
-   // printf("%i\n", ft_memcmp(str2, str1, -1));
-   // printf("%i", memcmp(str2, str1, -1));
+//    ret = memcmp(str1, str2, 5);
+//    printf("%i\n", ft_memcmp(str2, str1, -1));
+//    printf("%i", memcmp(str2, str1, -1));
 
-   // if(ret > 0) {
-   //    printf("str2 is less than str1\n");
-   // } else if(ret < 0) {
-   //    printf("str1 is less than str2\n");
-   // } else {
-   //    printf("str1 is equal to str2\n");
-   // }
+//    if(ret > 0) {
+//       printf("str2 is less than str1\n");
+//    } else if(ret < 0) {
+//       printf("str1 is less than str2\n");
+//    } else {
+//       printf("str1 is equal to str2\n");
+//    }
+//    ft_memcpy(str11, "abcdef", 6);
+//    ft_memcpy(str12, "ABCDEF", 6);
+//    if(ret1 > 0) {
+//       printf("str12 is less than str11");
+//    } else if(ret1 < 0) {
+//       printf("str11 is less than str12");
+//    } else {
+//       printf("str11 is equal to str12");
+//    }
+//    // char a[] = "\0aaa";
+//    // funk(a);
 
-
-   // ft_memcpy(str11, "abcdef", 6);
-   // ft_memcpy(str12, "ABCDEF", 6);
-
-   // ret1 = ft_memcmp(str1, str2, 5);
-
-   // if(ret1 > 0) {
-   //    printf("str12 is less than str11");
-   // } else if(ret1 < 0) {
-   //    printf("str11 is less than str12");
-   // } else {
-   //    printf("str11 is equal to str12");
-   // }
-   
-//    return(0);
 // }
 // 		DONE 
