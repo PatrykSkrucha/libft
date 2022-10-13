7531f09bfc85bec1c0ef6901b26698291f607c73
Patryk Skrucha <pskrucha@f1r4s10.codam.nl>
2022-10-13 12:57:35 +0200

    tests

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,21 +12,20 @@
 
 #include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char *dest_ptr;
-	char *src_ptr;
-	size_t i;
+	char	*dest_ptr;
+	char	*src_ptr;
+	size_t	i;
 
 	src_ptr = (char *)src;
 	dest_ptr = (char *)dest;
 	i = 0;
-
-	if( dest == NULL && src == NULL)
+	if (dest_ptr == NULL && src_ptr == NULL)
 		return (0);
-	if(src_ptr > dest_ptr)
+	if (src_ptr > dest_ptr)
 	{
-		while(i < n)
+		while (++i < n)
 		{
 			dest_ptr[i] = src_ptr[i];
 			i++;
@@ -34,27 +33,33 @@ void *ft_memmove(void *dest, const void *src, size_t n)
 	}
 	else
 	{
-		while(n)
-		{		
+		while (n > 0)
+		{
 			n--;
 			dest_ptr[n] = src_ptr[n];
 		}
 	}
-		
-
 	return ((void *)dest_ptr);
 }
 
+// void funk()
+// {
+// 	int i = 0;
+// 	while(i++<3)
+// 		printf("Hello");
+// }
+
 // int main()
 // {
-// 	char a[] = "abcd";
-// 	char a1[] = "abcd";
-// 	// char b[] = "abcde";
-// 	// char b1[] = "abcde";
-// 	printf("before: %s\n", a);
-// 	ft_memmove(a1, a1+2, 3);
-// 	memmove(a, a+2, 3);
-// 	printf("or: %s\n", a);
-// 	printf("mine: %s\n", a1);
+	// char a[] = "abcd";
+	// char a1[] = "abcd";
+	// // char b[] = "abcde";
+	// // char b1[] = "abcde";
+	// printf("before: %s\n", a);
+	// ft_memmove(a1, a1+2, 3);
+	// memmove(a, a+2, 3);
+	// printf("or: %s\n", a);
+	// printf("mine: %s\n", a1);
+// 	funk();
 
 // }
