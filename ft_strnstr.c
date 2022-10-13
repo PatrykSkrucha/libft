50aa9e6600a863babb57874b85912373923fc635
Patryk Skrucha <pskrucha@f1r4s10.codam.nl>
2022-10-13 20:13:57 +0200

    final fix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 12:52:17 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/11 18:46:31 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/13 20:13:43 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,21 @@ char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	size_t to_find_size;
 
-	to_find_size = ft_strlen(to_find);
 	i = 0;
 	j = 0;
 	if (to_find[j] == '\0' || len == 0)
 		return ((char *)str);
 	while (str[i] != '\0')
 	{
-		while (str[i + j] == to_find[j] && str[i + j] != '\0' && (int)i < (int)len)
+		while (str[i + j] == to_find[j] && i + j < len)
 		{
 			j++;
-			to_find_size--;
-			if (to_find[j] == '\0' && !to_find_size)
+			if (to_find[j] == '\0')
 				return ((char *)str + i);
 		}
-		to_find_size = ft_strlen(to_find);
 		i++;
 		j = 0;
 	}
 	return (NULL);
 }
-
-// int main()
-// {
-// 	char str[] = "Hello";
-// 	char tofind[] = "llo";
-// 	size_t a = 3;
-// 	puts(ft_strnstr(str, tofind, a));
-// 	puts(strnstr(str, tofind, a));
-// }
