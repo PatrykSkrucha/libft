8d44f24c33d468e4d75023b6d2c13e23b4519340
Patryk Skrucha <pskrucha@f1r4s10.codam.nl>
2022-10-13 13:12:30 +0200

    nor fix

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,43 +6,20 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 15:15:11 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/12 15:16:22 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/13 13:12:01 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void *ft_strchr(const char *str, int c)
-// {
-// 	size_t		i;
-// 	int x;
-
-// 	i = 0;
-// 	x = 0;
-
-// 	while (*(str + i) != c)
-// 	{
-// 		if (str[i] == c)
-// 			return ((void *) str + i);
-// 		i++;
-// 	}
-
-// 		return (NULL);
-
-// }
-
-char *ft_strchr(const char *str, int c)
+char	*ft_strchr(const char *str, int c)
 {
 	size_t		i;
-	// const char	*ptr;
 
-	// ptr = (char *)str;
 	i = 0;
-	// if (*str == '\0' && c != '\0')
-	// 	return (NULL);
-	if(c == '\0')
+	if (c == '\0')
 	{
-		while(*str)
+		while (*str)
 		{
 			str++;
 		}
@@ -68,9 +45,9 @@ char *ft_strchr(const char *str, int c)
 // }
 
 // int main () {
-// 	const char str[] = "http://www.tutorialspoint.com";
+// 	const char str[] = "\0http://www.tutorialspoint.com";
 // 	const char ch = '\0';
-// 	const char str1[] = "http://www.tutorialspoint.com";
+// 	const char str1[] = "\0http://www.tutorialspoint.com";
 // 	const char ch1 = '\0';
 // 	char *ret;
 // 	char *ret1;
@@ -80,8 +57,6 @@ char *ft_strchr(const char *str, int c)
 
 // 	printf("String after |%c| is - |%s|\n", ch, ret);
 // 	printf("String after |%c| is - |%s|\n", ch1, ret1);
-	
-	
-	
+
 // 	return(0);
 // }
