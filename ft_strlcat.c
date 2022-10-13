37b76ba9078278bfe3e4048d9c63a21de45d4caf
Patryk Skrucha <pskrucha@f1r4s10.codam.nl>
2022-10-13 14:35:18 +0200

    fixed ret value

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 17:17:46 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/11 14:33:43 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/13 14:35:02 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,19 @@ size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 		i++;
 	}
 	dst[i] = '\0';
-	return (ft_strlen(dst) + ft_strlen(src));
+	return (ft_strlen(dst) + dstsize);
 }
 // int main()
 // {
-// 	char dst[100] = "bb\0";
-// 	char src[100] = "aa";
-// 	char dst1[100] = "bb";
-// 	char src1[100] = "aa";
-// 	printf("moja: %lu %s\n", ft_strlcat(dst, src, 1), dst);
-// 	printf("or: %lu %s", strlcat(dst1, src1, 1), dst1);
+// 	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
+// 	char buff1[0xF00] = "there is no stars in the sky";
+// 	char buff2[0xF00] = "there is no stars in the sky";
+// 	size_t max = 0;
+// 	size_t i = strlcat(buff1, str, max);
+// 	size_t y = ft_strlcat(buff2, str, max);
+// 	printf("or size:		%zu\n", i);
+// 	printf("mine size:		%zu\n", y);
+// 	printf("or: string		%s\n", buff1);
+// 	printf("mine: string	%s\n", buff2);
 // 	return (0);
 // }
