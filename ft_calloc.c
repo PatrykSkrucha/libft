584e6984250e51958c3bdfd00dd54048e82a8aca
Patryk Skrucha <pskrucha@f1r4s20.codam.nl>
2022-10-14 16:07:47 +0200

    final fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,14 +6,33 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:47:28 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/11 18:48:30 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/14 16:07:29 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
-// void    *calloc(size_t nelem, size_t elsize)
-// {
+void	*ft_calloc(size_t nelem, size_t elsize)
+{
+	void	*a;
 
+	a = (void *)malloc(nelem * elsize);
+	if (a == NULL)
+		return (NULL);
+	ft_bzero(a, nelem * elsize);
+	return (a);
+}
+
+// int main () {
+// 	int size = 8539;
+
+// 	void * d1 = ft_calloc(size, sizeof(int));
+// 	void * d2 = calloc(size, sizeof(int));
+// 	if (memcmp(d1, d2, size * sizeof(int)))
+// 		printf("sukces");
+// 	else
+// 		printf("fial");
+// 	free(d1);
+// 	free(d2);
+//    return(0);
 // }
-
