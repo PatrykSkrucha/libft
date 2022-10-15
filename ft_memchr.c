ba5764b909f3543854430b83e702e4dacaf5413d
Patryk Skrucha <pskrucha@f0r2s2.codam.nl>
2022-10-15 19:34:03 +0200

    final fix

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:49:45 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/11 16:13:57 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/15 19:33:31 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 void	*ft_memchr(const void *str, int c, size_t n)
 {
 	size_t		i;
-	const char	*ptr;
+	char		*ptr;
+	char		a;
 
 	ptr = (char *)str;
 	i = 0;
-	if (*ptr == '\0')
-		return (NULL);
+	a = c;
 	while (i < n)
 	{
-		if (ptr[i] == c)
+		if (ptr[i] == a)
 			return ((void *) ptr + i);
 		i++;
 	}
@@ -41,6 +41,8 @@ void	*ft_memchr(const void *str, int c, size_t n)
 //    printf("orString after |%c| is - |%s|\n", ch, ret);
 
 //    printf("mojaString after |%c| is - |%s|\n", ch, ret1);
-
-//    return(0);
-// }                                DONE
+// 	char s[] = {1, 0, 1, 2 ,3 ,4 ,5};
+// 	// printf("%s\n",ft_memchr(s, 2 + 256, 3));
+// 	printf("%c", s[1]);
+// 	return(0);
+// }                                
