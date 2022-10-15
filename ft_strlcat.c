93d02699ed8d0f2d1d41f32dd34ec32b2ed58eec
Patryk Skrucha <pskrucha@f0r2s2.codam.nl>
2022-10-15 20:57:41 +0200

    test 5 to fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,12 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 17:17:46 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/15 14:59:30 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/15 20:57:22 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {	
@@ -20,9 +21,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	j = 0;
 	i = 0;
-	x = i;
 	if (dst)
 		i = (int)ft_strlen(dst);
+	x = i;
 	if ((int)dstsize <= i)
 		return (ft_strlen(src) + dstsize);
 	if ((int)dstsize > i)
@@ -43,23 +44,19 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 // int main()
 // {
-// 	char a[0xF] = "nyan !";
-	// char b[0xF] = "nyan !";
-
-	// printf("%s\n", a);
-	// strlcat(((void *)0), a, 0);
-	// ft_strlcat(((void *)0), b, 0);
-	// printf("%zu\n", ft_strlcat(((void *)0), a, 0));
-	// printf("mine: 	%s", b);
-// 	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
-// 	char buff1[0xF00] = "there is no stars in the sky";
-// 	char buff2[0xF00] = "there is no stars in the sky";
-// 	size_t max = 50;
-// 	int b = strlcat(buff1, str, max);
-// 	int a = ft_strlcat(buff2, str, max);
-// 	printf("mine size:		%d\n", a);
-// 	printf("or size:		%d\n", b);
-// 	printf("or: string		%s\n", buff1);
-// 	printf("mine: string		%s\n", buff2);
+// 	char dest[30]; 
+// 	memset(dest, 0, 30);
+// 	char * src = (char *)"AAAAAAAAA";
+// 	dest[0] = 'B';
+// 	strlcat(dest, src, 0);
+// 	dest[0] = 'B';
+// 	ft_strlcat(dest, src, 1);
+// 	memset(dest, 'B', 4);
+// 	ft_strlcat(dest, src, 3);
+// 	ft_strlcat(dest, src, 6);
+// 	memset(dest, 'C', 5);
+// 	// printf("size ret: %zu", (strlen(dest) + strlen(src)));
+// 	// printf("%zu", ft_strlcat(dest, src, -1));
+// 	printf("%s", dest);
 // 	return (0);
 // }
