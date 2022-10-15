23a3d279a74dbe9c7d86b4a8be1899b86c8e5224
Patryk Skrucha <pskrucha@f0r2s2.codam.nl>
2022-10-15 19:36:39 +0200

    final fix

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 15:02:08 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/14 13:42:57 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/15 19:35:44 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 char	*ft_strrchr(const char *str, int c)
 {
 	size_t	i;
+	char	a;
 
 	i = ft_strlen(str);
+	a = c;
 	while ((int)i >= 0)
 	{
-		if (str[i] == c)
+		if (str[i] == a)
 			return ((char *) str + i);
 		i--;
 	}
