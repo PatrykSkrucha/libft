059691dd0dbad7c1f2472fd05f3940e7e5c6c57c
Patryk Skrucha <pskrucha@f0r2s2.codam.nl>
2022-10-15 19:31:42 +0200

    final fix

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 15:15:11 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/13 13:12:01 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/15 19:30:58 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-	size_t		i;
+	char	a;
+	int		i;
 
 	i = 0;
-	if (c == '\0')
+	a = c;
+	if (a == '\0')
 	{
 		while (*str)
 		{
@@ -27,22 +29,12 @@ char	*ft_strchr(const char *str, int c)
 	}
 	while (str[i] != '\0')
 	{
-		if (str[i] == c)
+		if (str[i] == a)
 			return ((char *) str + i);
 		i++;
 	}
 	return (NULL);
 }
-
-// void ft_funk(char *str)
-// {
-// 	int i = 0;
-// 	while(*str + i)
-// 	{
-// 		printf("jest");
-// 		i++;
-// 	}
-// }
 
 // int main () {
 // 	const char str[] = "\0http://www.tutorialspoint.com";
@@ -57,6 +49,8 @@ char	*ft_strchr(const char *str, int c)
 
 // 	printf("String after |%c| is - |%s|\n", ch, ret);
 // 	printf("String after |%c| is - |%s|\n", ch1, ret1);
+// 	char s[] = "tripouille";
+// 	puts(ft_strchr(s, 't' + 256));
 
 // 	return(0);
 // }
