3e542c7537acb1350b16620fde58a351c5c560d5
Patryk Skrucha <pskrucha@f0r2s2.codam.nl>
2022-10-15 14:36:56 +0200

    your strlcat crush when null parameter is sent with a size of 0

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,41 +6,52 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 17:17:46 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/13 16:35:51 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/15 14:36:24 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {	
 	int	i;
 	int	j;
+	size_t x;
 
 	j = 0;
-	i = (int)ft_strlen(dst);
-	dstsize -= i;
-	while (src[j] != '\0' && j < (int)dstsize - 1)
+	i = ft_strlen(dst);
+	x = i;
+	if((int)dstsize < i)
+		return (ft_strlen(src) + dstsize);
+	if ((int)dstsize > i)
 	{
-		dst[i] = src[j];
-		j++;
-		i++;
+		dstsize -= i;
+
+			while (src[j] != '\0' && j < (int)dstsize - 1)
+			{
+				dst[i] = src[j];
+				j++;
+				i++;
+			}
+			dst[i] = '\0';
 	}
-	dst[i] = '\0';
-	return (ft_strlen(dst) + dstsize);
+	if((int)dstsize > j)
+		return (ft_strlen(src) + x);
+	return (ft_strlen(src) + ft_strlen(dst));
 }
-int main()
-{
-	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
-	char buff1[0xF00] = "there is no stars in the sky";
-	char buff2[0xF00] = "there is no stars in the sky";
-	size_t max = 10;
-	size_t y = ft_strlcat(buff2, str, max);
-	size_t i = strlcat(buff1, str, max);
-	printf("or size:		%zu\n", i);
-	printf("mine size:		%zu\n", y);
-	printf("mine before: string	%s\n", buff2);
-	printf("or: string		%s\n", buff1);
-	printf("mine: string	%s\n", buff2);
-	return (0);
-}
+
+
+// int main()
+// {
+// 	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
+// 	char buff1[0xF00] = "there is no stars in the sky";
+// 	char buff2[0xF00] = "there is no stars in the sky";
+// 	size_t max = 50;
+// 	int b = strlcat(buff1, str, max);
+// 	int a = ft_strlcat(buff2, str, max);
+// 	printf("mine size:		%d\n", a);
+// 	printf("or size:		%d\n", b);
+// 	printf("or: string		%s\n", buff1);
+// 	printf("mine: string		%s\n", buff2);
+// 	return (0);
+// }
