88ecc00d0f9272eb43f21205b8de8a36a35a7d2e
Patryk Skrucha <patryk.skrucha@gmail.com>
2022-10-18 17:09:29 +0200

    final fix

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,28 +6,25 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 13:53:10 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/18 14:33:09 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/18 17:08:26 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putchar(char c, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-    write(fd, &c, 1);
-}
+	char	a;
 
-void ft_putnbr_fd(int n, int fd)
-{
-    if (n == -2147483648)
+	if (n == -2147483648)
 	{
-		ft_putchar('-', fd);
-		ft_putchar('2', fd);
+		write(fd, "-", 1);
+		write(fd, "2", 1);
 		ft_putnbr_fd(147483648, fd);
 	}
 	else if (n < 0)
 	{
-		ft_putchar('-', fd);
+		write(fd, "-", 1);
 		ft_putnbr_fd(-n, fd);
 	}
 	else if (n > 9)
@@ -36,6 +33,8 @@ void ft_putnbr_fd(int n, int fd)
 		ft_putnbr_fd(n % 10, fd);
 	}
 	else
-		ft_putchar(n + '0', fd);
+	{
+		a = n + '0';
+		write(fd, &a, 1);
+	}
 }
-
