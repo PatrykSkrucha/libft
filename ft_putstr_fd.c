563ca54aebcfaa6472697fa5178a75f78171fbb4
Patryk Skrucha <patryk.skrucha@gmail.com>
2022-10-18 13:34:25 +0200

    some fixed

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/18 12:56:48 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/18 13:33:18 by pskrucha         ###   ########.fr       */
+/*   Created: 2022/10/18 13:28:04 by pskrucha          #+#    #+#             */
+/*   Updated: 2022/10/18 13:33:59 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putchar_fd(char c, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, &c, 1);
+	if (s)
+		write(fd, s, ft_strlen(s));
 }
