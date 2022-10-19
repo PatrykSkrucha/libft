c4b06545f7151fc763fd1bf1873d1db799cf9b71
Patryk Skrucha <patryk.skrucha@gmail.com>
2022-10-19 13:44:14 +0200

    fixed

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 17:51:23 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/19 13:18:00 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/19 13:42:36 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,21 +44,32 @@ char *ft_itoa(int n)
 	int	s;
 	char *str = "aa";
 	int sign;
+	int min_value;
 
 	s = str_size(n);
-
+	min_value = 0;
 	str = (char *) malloc(s);
 	if(str == NULL)
 		return (NULL);
 	sign = 0;
 	s--;
 	str[s] = '\0';
-	if(n < 0)
+	if (n == -2147483648)
+	{
+		min_value++;
+		n = -214748364;
+	}
+	if (n < 0)
 	{
 		n *= -1;
 		sign++;
 	}
-	while(n > 0)
+	if(min_value)
+	{
+		s--;
+		str[s] = '8';
+	}
+	while(n >= 1)
 	{	
 		s--;
 		str[s] = n %10 + '0';
@@ -77,9 +88,8 @@ char *ft_itoa(int n)
 	return (str);
 }
 
-int main()
-{
-	char *s = ft_itoa(2);
-	puts(s);
-	free(s);
-}
+// int main()
+// {
+// 	char *s = ft_itoa(156);
+// 	puts(s);
+// }
