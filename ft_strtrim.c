d0afdc15b3c8ae76cc2f0339b9f80cff76845810
Patryk Skrucha <patryk.skrucha@gmail.com>
2022-10-19 16:32:04 +0200

    initial commit

---
+++ ft_strtrim.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 14:40:07 by pskrucha          #+#    #+#             */
+/*   Updated: 2022/10/19 16:29:10 by pskrucha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	move_start(char const *s1, char const *set, int start)
+{
+	int i;
+
+	i = 0;
+	while (s1[start] && set[i])
+	{
+		if (s1[start] == set[i])
+		{
+			i = 0;
+			start++;
+			return (1);	
+		}
+		i++;
+	}
+	return (0);
+}
+
+int move_end(char const *s1, char const *set, int end)
+{
+	int i;
+
+	i = 0;
+	while (s1[end] && set[i])
+	{
+		if (s1[end] == set[i])
+		{
+			i = 0;
+			end--;
+			return (1);	
+		}
+		i++;
+	}
+	return (0);
+}
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	int start;
+	int end;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while(move_start(s1, set, start))
+	{
+		start++;
+	}
+	while(move_end(s1, set, end))
+	{
+		end--;
+	}
+	if (start > end + 1)
+		return (ft_strdup(""));
+	str = ft_substr(s1, start, end - start + 1);
+	return (str);
+}
+
+// int main()
+// {
+// 	char const *s1 = "HaeH";
+// 	char const *set = "He";
+// 	puts(ft_strtrim(s1, set));
+// }
