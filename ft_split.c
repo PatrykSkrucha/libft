f2a8d7c18125f4b78d6c2b4cbbd7104c78f9191d
Patryk Skrucha <patryk.skrucha@gmail.com>
2022-10-21 14:58:54 +0200

    some chagnes

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 16:37:30 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/21 12:30:38 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/21 14:57:24 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	count_words(char const *s1, char c)
 		{
 			check = 1;
 		}
-		if (s1[i] != c && check)
+		if ((s1[i] != c && check) || (!check && i == 0) )
 		{
 			counter++;
 			check = 0;
@@ -37,18 +37,79 @@ int	count_words(char const *s1, char c)
 	return (counter);
 }
 
+int count_position(char const *s1, char c, int i, int start_or_end)
+{
+
+
+	while (s1[i])
+	{
+		if(start_or_end)
+		{
+			if (s1[i] != c)
+				return (i);
+			i++;
+		}
+		else
+		{
+			if (s1[i] == c)
+				return (i);
+			i++;
+		}
+	}
+}
+
 // char **ft_split(char const *s, char c)
 // {
+// 	char **str;
+// 	int words;
+// 	int start;
+// 	int end;
 
-// 	char **str = (char **)malloc(4);
-
+// 	if (!s)
+// 		return (NULL);
+// 	start = 0;
+// 	end = 0;
+// 	words = count_words(s, c);
+// 	str = (char **) malloc(words + 1);
+// 	if (!str)
+// 		return (NULL);
+// 	start = count_position(s, c, start, 1);
+// 	end = count_position(s, c, start, 0);
+	
 // 	return (str);
 // }
 
+char **dummy(char const *s, char c)
+{
+	char **str;
+	int words;
+	int start;
+	int end;
+
+	if (!s)
+		return (NULL);
+	start = 0;
+	end = 0;
+	words = count_words(s, c);
+	str = (char **) malloc(words + 1);
+	if (!str)
+		return (NULL);
+	while (words)
+	{
+		start = count_position(s, c, end, 1);
+		end = count_position(s, c, start, 0);
+		printf("start: %i\nend: %i\n", start, end);
+		words--;
+	}
+	free(str);
+	return (str);
+}
+
 int main ()
 {
-// 	char *string = "      split       this for   me  !       ";
+	char *string = "      split       this for   me  !       ";
 // 	char **expected = ((char*[6]){"split", "this", "for", "me", "!", ((void *)0)});
 // 	char **result = ft_split(string, ' ');
-	printf("%i", count_words("aan", 'n'));
+	dummy(string, ' ');
+	
 }
