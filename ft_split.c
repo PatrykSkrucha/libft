cceda64ad52ab92b19a86b352fbd487eef2f1adb
Patryk Skrucha <patryk.skrucha@gmail.com>
2022-10-23 13:36:08 +0200

    norminette fixed

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 16:37:30 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/23 13:30:47 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/23 13:35:46 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 static int	count_words(char const *s1, char c)
 {
 	int	i;
-	int counter;
-	int check;
+	int	counter;
+	int	check;
 
 	check = 0;
 	counter = 0;
@@ -27,7 +27,7 @@ static int	count_words(char const *s1, char c)
 		{
 			check = 1;
 		}
-		if ((s1[i] != c && check) || (!check && i == 0) )
+		if ((s1[i] != c && check) || (!check && i == 0))
 		{
 			counter++;
 			check = 0;
@@ -37,11 +37,11 @@ static int	count_words(char const *s1, char c)
 	return (counter);
 }
 
-static unsigned int count_position(char const *s1, char c, int i, int control)
+static int	count_position(char const *s1, char c, int i, int control)
 {
 	while (s1[i])
 	{
-		if(control)
+		if (control)
 		{
 			if (s1[i] != c)
 				return (i);
@@ -53,12 +53,12 @@ static unsigned int count_position(char const *s1, char c, int i, int control)
 		}
 			i++;
 	}
-	if(!control)
+	if (!control)
 		return (ft_strlen(s1));
 	return (0);
 }
 
-static void clearall(char **s, int i)
+static void	clearall(char **s, int i)
 {
 	while (i >= 0)
 	{
@@ -68,11 +68,11 @@ static void clearall(char **s, int i)
 	free(s);
 }
 
-static int fill_string(int words, char const *s, char c, char **str)
+static int	fill_string(int words, char const *s, char c, char **str)
 {
-	unsigned int start;
-	unsigned int end;
-	int i;
+	int	start;
+	int	end;
+	int	i;
 
 	i = 0;
 	start = 0;
@@ -93,34 +93,18 @@ static int fill_string(int words, char const *s, char c, char **str)
 	return (1);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	char **str;
-	int words;
+	char	**str;
+	int		words;
 
 	if (!s)
-	{
 		return (NULL);
-	}
-	
 	words = count_words(s, c);
-	str =(char **) malloc((words + 1) * sizeof(char*));
+	str = (char **) malloc((words + 1) * sizeof(char *));
 	if (!str)
 		return (NULL);
-	// while (i < words)
-	// {
-	// 	start = count_position(s, c, end, 1);
-	// 	end = count_position(s, c, start, 0);
-	// 	str[i] = ft_substr(s, start, end - start);
-	// 	if (!str[i])
-	// 	{
-	// 		clearall(str, i);
-	// 		return (NULL);
-	// 	}
-	// 	i++;
-	// }
-	// str[i] = 0;	
-	if(!fill_string(words, s, c, str))
+	if (!fill_string(words, s, c, str))
 		return (NULL);
 	return (str);
 }
@@ -141,5 +125,3 @@ char **ft_split(char const *s, char c)
 
 // free(s);
 // }
-
-//gcc -Wall -Werror -Wextra ft_split.c ft_strlen.c ft_substr.c ft_strdup.c ft_calloc.c ft_strlcpy.c ft_bzero.c ft_memset.c && ./a.out
