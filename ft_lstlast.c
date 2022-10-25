d92f7c1f1eea8f3f9b2c2d1798d322626be7dd33
Patryk Skrucha <patryk.skrucha@gmail.com>
2022-10-25 19:41:08 +0200

    norminette fix

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,19 +6,16 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 13:57:39 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/25 15:39:53 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/25 19:40:47 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstlast(t_list *lst)
+t_list	*ft_lstlast(t_list *lst)
 {
-	if(lst == NULL)
+	if (lst == NULL)
 		return (NULL);
-	// if (lst->next == NULL)
-	// 	return (lst);
-	// return (ft_lstlast(lst->next));
 	while (lst->next != NULL)
 		lst = lst->next;
 	return (lst);
