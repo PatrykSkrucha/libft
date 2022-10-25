868029dafe89e9825d75906c810223fbc68d4669
Patryk Skrucha <patryk.skrucha@gmail.com>
2022-10-25 16:59:05 +0200

    initial commit

---
+++ ft_lstclear.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 13:59:50 by pskrucha          #+#    #+#             */
+/*   Updated: 2022/10/25 16:58:54 by pskrucha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	while (*lst)
+	{
+		temp = *lst;
+		*lst = temp->next;
+		del(temp->content);
+		free(temp);
+	}
+}
