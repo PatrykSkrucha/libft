c601dd4de4ccbf48af3ddbfb96d5098a3ca7fb03
Patryk Skrucha <patryk.skrucha@gmail.com>
2022-10-26 13:41:12 +0200

    fixed

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 14:02:01 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/26 12:41:30 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/10/26 13:14:31 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,13 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	{
 		el = ft_lstnew(f(lst->content));
 		if (el)
-		{
 			ft_lstadd_back(&newlst, el);
-			lst = lst->next;
-		}
 		else
 		{
 			ft_lstclear(&newlst, del);
-			return (0);
+			return (NULL);
 		}
+			lst = lst->next;
 	}
 	return (newlst);
 }
