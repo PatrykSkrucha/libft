02484a8a2532a2271f55f1832573fb3c2d77cc62
Patryk Skrucha <pskrucha@f0r2s2.codam.nl>
2022-11-03 15:35:55 +0100

    fixed strict

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: pskrucha <pskrucha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 14:02:01 by pskrucha          #+#    #+#             */
-/*   Updated: 2022/10/26 13:14:31 by pskrucha         ###   ########.fr       */
+/*   Updated: 2022/11/03 15:35:17 by pskrucha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlst;
 	t_list	*el;
+	void	*content;
 
 	if (!lst)
 		return (0);
 	newlst = 0;
 	while (lst)
 	{
-		el = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		el = ft_lstnew(content);
 		if (el)
 			ft_lstadd_back(&newlst, el);
 		else
 		{
+			del(content);
 			ft_lstclear(&newlst, del);
 			return (NULL);
 		}
